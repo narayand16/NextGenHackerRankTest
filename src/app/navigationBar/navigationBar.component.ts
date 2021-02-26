2228632af4a9533461d8bb7d5d61e9ce8ade9f4a
Narayan Deshmukh <narayandeshmukh16@gmail.com>
2021-02-26 15:07:31 +0530

    initial commit

--- src/app/navigationBar/navigationBar.component.ts
+++ src/app/navigationBar/navigationBar.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'navigation-bar',
@@ -7,7 +7,33 @@ import {Component, OnInit} from '@angular/core';
 })
 export class NavigationBar implements OnInit {
 
+  tabContent = 'HOME PAGE';
+
   ngOnInit() {
+  }
+
+  showContent(tab) {
+    switch (tab) {
+      case 'home': {
+        this.tabContent = 'HOME PAGE';
+        break;
+      }
+      case 'about': {
+        this.tabContent = 'ABOUT PAGE';
+        break;
+      }
+      case 'news': {
+        this.tabContent = 'NEWS PAGE';
+        break;
+      }
+      case 'contact': {
+        this.tabContent = 'CONTACT PAGE';
+        break;
+      }
+      default: {
+        this.tabContent = 'HOME PAGE';
+      }
+    }
   }
 
 }
